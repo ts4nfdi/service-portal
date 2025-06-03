a8f0822f680c7c6e930f72b29d72479790be9301
pooya.oa <pooya.oladazimi@tib.eu>
2025-06-03 16:27:20 +0200

    change nodemailer config

--- app/libs/email.ts
+++ app/libs/email.ts
@@ -28,20 +28,17 @@ type EmailMessage = {
 
 export async function sendEmail(props: InputProps): Promise<boolean> {
   console.log("host name: ", process.env.SMTP_HOST_NAME)
+  console.log("host port: ", process.env.SMTP_PORT)
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST_NAME,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     //auth: {
     //user: process.env.SMTP_USERNAME,
     //pass: process.env.SMTP_PASSWORD,
     //},
-    name: process.env.NEXTAUTH_URL! as string,
-    connectionTimeout: 5000,
-    greetingTimeout: 3000,
-    tls: {
-      rejectUnauthorized: false,
-    },
+    //name: process.env.NEXTAUTH_URL! as string,
+    connectionTimeout: 10000,
   } as SMTPTransport.Options);
 
 
