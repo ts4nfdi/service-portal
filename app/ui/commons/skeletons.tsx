b2b4b1e20949d49736a4dbbbd6ced7792280c8ce
pooya.oa <pooya.oladazimi@tib.eu>
2025-08-12 12:49:17 +0200

    add card skeleton for collection list

--- app/ui/commons/skeletons.tsx
+++ app/ui/commons/skeletons.tsx
@@ -14,3 +14,23 @@ export function DefaultSkeleton(props: { lineCount: number, className?: string }
         </div>
     );
 }
+
+export function CardSkeleton(props: { count: number, className?: string }) {
+    let cards: React.ReactNode[] = [];
+    for (let i = 1; i < props.count; i++) {
+        cards.push(
+            <div className="bg-white rounded border-ts4nfdi-brand-color p-4  dark:bg-gray-800">
+                <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
+                <div className="h-4 bg-gray-300 rounded w-1/2 mb-2"></div>
+                <div className="h-4 bg-gray-300 rounded w-1/2 mb-2"></div>
+                <div className="h-4 bg-gray-300 rounded w-1/2 mb-2"></div>
+                <div className="h-4 bg-gray-300 rounded w-full mb-2"></div>
+            </div>
+        );
+    }
+    return (
+        <div className={"animate-pulse space-y-4 " + (props.className ? props.className : "")}>
+            {cards}
+        </div>
+    )
+}
