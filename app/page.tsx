cefd1ae556aae4c8c57cd26e730748d78750901b
rombaum <r.baum@zfmk.de>
2025-11-04 09:22:14 +0100

    feat(main): WIP Reordering main page.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <>
-            <div className='md:col-span-1 mx-auto'>
+            <div className='bg-red-500 md:col-span-3 max-h-[120px] text-right '>
+                <h1 className='header-1'>What is TS4NFDI?</h1>
+            </div>
+            <div className=''>
                 <Image
                     src={"/logo.png"}
                     width={400}
@@ -14,17 +17,14 @@ export default function Home() {
                     blurDataURL="/blur.webp"
                 />
             </div>
-            <div className='md:col-span-2'>
+            <div className='md:col-span-2 bg-yellow-500 min-h-[50px]'>
                 <p className='text-justify'>
                     Terminology Services 4 NFDI (TS4NFDI) is a cross-domain service for the provision, curation,
-                    development,
-                    harmonization, and mapping of terminologies. It aims to facilitate consensus-building and
-                    interoperability
-                    of services across disciplines to achieve a shared knowledge representation and knowledge
-                    engineering
-                    framework. The service seeks to integrate and converge individual solutions into a standardized,
-                    interoperable, and sustainable service suite with Service Wrapper, API Gateway, mapping service,
-                    and reusable GUI widgets.
+                    development, harmonization, and mapping of terminologies. It aims to facilitate consensus-building
+                    and interoperability of services across disciplines to achieve a shared knowledge representation and
+                    knowledge engineering framework. The service seeks to integrate and converge individual solutions
+                    into a standardized, interoperable, and sustainable service suite with Service Wrapper, API Gateway,
+                    mapping service, and reusable GUI widgets.
                 </p>
             </div>
         </>
