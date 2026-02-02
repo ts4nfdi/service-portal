c4303b54bb8df862ba678a1acc131959cddf828e
pooya.oa <pooya.oladazimi@tib.eu>
2026-02-02 13:07:22 +0100

    use auth token for user list action

--- app/api/actions/users.ts
+++ app/api/actions/users.ts
@@ -2,13 +2,18 @@
 
 import { ActionResponse } from "./types";
 import { getHttpHeaderForGateway } from "@/app/libs/server_utils";
-import { REQUEST_FAILED_MESSAGE, SERVER_ERROR_MESSAGE } from "@/app/libs/responseStrings";
+import { REQUEST_FAILED_MESSAGE, SERVER_ERROR_MESSAGE, ACTION_NOT_ALLOWED_MESSAGE } from "@/app/libs/responseStrings";
+import { getUserToken } from "@/app/libs/auth";
 
 
 export async function getUserList(): Promise<ActionResponse> {
   try {
+    let token = await getUserToken();
+    if (!token) {
+      return { status: false, content: ACTION_NOT_ALLOWED_MESSAGE }
+    }
     let resp = await fetch((process.env.GATEWAY_BASE_URL! as string) + "/users/", {
-      headers: await getHttpHeaderForGateway()
+      headers: await getHttpHeaderForGateway(token)
     });
     if (!resp.ok) {
       return { status: false, content: REQUEST_FAILED_MESSAGE }
