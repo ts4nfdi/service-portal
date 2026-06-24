ee3b2addf8fcd867562817161a101254bf8cc57f
pooya.oa <pooya.oladazimi@tib.eu>
2026-06-24 13:16:45 +0200

    add agents config and fix the issue with test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,60 +1,40 @@
-import {defineConfig, devices} from '@playwright/test';
-import dotenv from 'dotenv';
-import path from 'path';
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({path: path.resolve(__dirname, '.env')});
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export default defineConfig({
-    testDir: './tests',
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
-    use: {
-        trace: 'on-first-retry',
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
-    projects: [
-        {
-            name: 'chromium',
-            use: {...devices['Desktop Chrome']},
-        },
-
-        {
-            name: 'firefox',
-            use: {...devices['Desktop Firefox']},
-        },
-
-        {
-            name: 'webkit',
-            use: {...devices['Desktop Safari']},
-        },
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
-    ],
-
-    webServer: {
-        command: 'npm run dev',
-        url: 'http://localhost:3000',
-        reuseExistingServer: !process.env.CI,
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
--- tests/libs.ts
+++ tests/libs.ts
@@ -1,5 +1,11 @@
 import {Page} from '@playwright/test';
 
+export async function acceptTrackingConsent(page: Page): Promise<void> {
+    await page.addInitScript(() => {
+        window.localStorage.setItem('matomoIsEnabled', 'true');
+    });
+}
+
 export async function isImageLoaded(page: Page, selectRule: string): Promise<boolean> {
     return await page.locator(selectRule).evaluate((img: HTMLImageElement) => {
         return img.complete && img.naturalWidth > 0;
