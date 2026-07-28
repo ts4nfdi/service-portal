af6763c388ad12ece542c2a75542c5aa4192069b
pooya.oa <pooya.oladazimi@tib.eu>
2026-07-28 17:25:45 +0200

    use prod url as default redirection url

--- app/api/sso/login/route.ts
+++ app/api/sso/login/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
   const redirectUri =
-    process.env.NEXT_IAM_REDIRECT_URL ?? request.nextUrl.origin;
+    process.env.NEXT_IAM_REDIRECT_URL ??
+    "https://terminology.services.base4nfdi.de/";
   const authorizeUrl = new URL(
     "/api-gateway/auth/sso/authorize",
     process.env.GATEWAY_BASE_URL,
