f0900416b84549505cc7531e6b3f06938b6a513f
pooya.oa <pooya.oladazimi@tib.eu>
2026-07-28 16:34:47 +0200

    use public var for redirection url

--- app/api/sso/login/route.ts
+++ app/api/sso/login/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
-  const redirectUri = process.env.IAM_REDIRECT_URL ?? request.nextUrl.origin;
+  const redirectUri =
+    process.env.NEXT_IAM_REDIRECT_URL ?? request.nextUrl.origin;
   const authorizeUrl = new URL(
     "/api-gateway/auth/sso/authorize",
     process.env.GATEWAY_BASE_URL,
